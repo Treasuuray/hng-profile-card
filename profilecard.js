08a0e38e342c78202c51d13ff13ed2c160597918
treasuuray <treasureolowolade@gmail.com>
2025-02-02 19:44:46 +0000

    my profile card

---
+++ profilecard.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+    setUTCTime()
+}, 1000);
+
+function setUTCTime() {
+const time = utcTime = new Date().toUTCString();
+document.getElementById('time').innerHTML = time;
+}
